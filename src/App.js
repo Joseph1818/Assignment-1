f91f6f15dc6f4c2708a3ef9576ffc69eca443033
Umba Joseph <43114099+Joseph1818@users.noreply.github.com>
2024-01-24 22:09:29 +0200

    Second commit, Api Async method call added and data displaying try 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from "./logo.svg";
+// App.js
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { useEffect, useState } from "react";
 
-function App() {
-  const [apiData, setApiData] = useState([]);
-  async function fetchData() {
-    try {
-      const response = await fetch(
-        "https://arthurfrost.qflo.co.za/php/getTimeline.php"
-      );
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const items = data.Timeline;
+    return items;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
   }
-  fetchData();
+};
 
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [apiData, setApiData] = useState([]);
+  const [imageBaseUrl, setImageBaseUrl] = useState("");
+  const [mp3BaseUrl, setMp3BaseUrl] = useState("");
+
+  useEffect(() => {
+    const fetchApiData = async () => {
+      const timelineUrl = "https://arthurfrost.qflo.co.za/php/getTimeline.php";
+      const imageBaseUrl = "https://arthurfrost.qflo.co.za/Images/";
+      const mp3BaseUrl = "https://arthurfrost.qflo.co.za/MP3/";
+
+      try {
+        const apiData = await fetchData(timelineUrl);
+        setApiData(apiData);
+        setImageBaseUrl(imageBaseUrl);
+        setMp3BaseUrl(mp3BaseUrl);
+      } catch (error) {
+        console.error("Error fetching timeline data:", error);
+      }
+    };
+    fetchApiData();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h1>TimeLine Data</h1>
+      <ul className="timeline-list">
+        {apiData.map((item) => (
+          <li key={item.id} className="timeline-item">
+            <p>Id: {item.Id}</p>
+            <p>Title: {item.Title}</p>
+            <p>Episode: {item.Episode}</p>
+            <p>CreateDate: {item.CreateDate}</p>
+            <p>Media Name : {item.MediaName}</p>
+            
+
+            {item.image && (
+              <img src={`${imageBaseUrl}${item.image}`} alt="Timeline Image" />
+            )}
+
+            {item.audio && (
+              <audio controls src={`${mp3BaseUrl}${item.audio}`} />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
